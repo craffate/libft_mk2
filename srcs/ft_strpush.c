68439f56398a45651d2edea797800ac6080a847a
craffate <craffate@student.42.fr>
2017-03-24 22:11:51 +0100

    Fixed Norme errors

--- srcs/ft_strpush.c
+++ srcs/ft_strpush.c
@@ -6,7 +6,7 @@
 /*   By: craffate <craffate@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/22 16:34:17 by craffate          #+#    #+#             */
-/*   Updated: 2017/03/22 16:50:39 by craffate         ###   ########.fr       */
+/*   Updated: 2017/03/24 22:11:30 by craffate         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ void	ft_strpush(char **s, const char c)
 	char	*str;
 	size_t	si;
 
-	str = ((si = ft_strlen(*s)) % MALLOCSIZE_STR) ? *s :
+	str =
+	((si = ft_strlen(*s)) % MALLOCSIZE_STR) ? *s :
 	(char *)malloc(sizeof(char) * (si + MALLOCSIZE_STR + 1));
 	str[si] = c;
 	str[si + 1] = 0;
