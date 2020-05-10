d90ec304288693b25fa7f1c5c6f99b081cfcaec7
Cyril Raffatelli <cyril.raffatelli@outlook.com>
2020-05-10 02:18:09 +0200

    Fix Norme errors

--- includes/libft.h
+++ includes/libft.h
@@ -19,14 +19,13 @@
 # include <limits.h>
 # include <wchar.h>
 
-#ifdef __linux__
-# include <stdio.h>
-# include <stdint.h>
-#endif
+# ifdef __linux__
+#  include <stdio.h>
+#  include <stdint.h>
+# endif
 
 # define MALLOCSIZE_STR 32
 # define MALLOCSIZE_ARR 4
-
 
 typedef struct		s_list
 {
